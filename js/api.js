b0542011adcb5f5922cb8e7bb2cc090074c31527
asciphx <maxspore@foxmail.com>
2019-10-14 19:47:25 +0800

    best in the world

---
+++ js/api.js
@@ -0,0 +1,70 @@
+function int10to62(n) {
+    const c = '0123456789ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz';var s = "";
+    do {
+        m = n % 62;
+        n = (n - m) / 62;
+        s = c.charAt(m) + s;
+    }
+    while (n);
+    return s;
+}
+function generateId() {
+    return 'xxx'.replace(/x/g, (c) => { return (Math.random() * 36 | 0).toString(36); })
+    + ("0x" + new Date().getTime().toString(16) << 1).toString(36).replace(/^-/, "")
+}
+
+function delOne(list, v) {
+    for (i in list) {
+        if (list[i].innerHTML === v) {
+            list[i].parentNode.removeChild(list[i])
+        }
+    }
+}
+function objParse(parent, v, b) {
+    var li = document.createElement("li"); li.innerHTML = v;
+    li.style = "background:#fff;border:1px solid #ccc";
+    if (b)
+        li.addEventListener("dblclick", function () {
+            if ('room'.Id().innerHTML === "") so.emit("create or join", v)
+            else alert("你已经在一个房间了！")
+        })
+    li.addIn(parent.Id());
+
+}
+function listParse(parent, v, b) {
+    parent = parent.Id(); parent.innerHTML = "";
+    for (i in v) {
+        var li = document.createElement("li"); li.innerHTML = v[i];
+        li.style = "background:#fff;border:1px solid #ccc";
+        if (b) {
+            b = v[i];
+            li.addEventListener("dblclick", function () {
+                if ('room'.Id().innerHTML === "") so.emit("create or join", b)
+                else alert("你已经在一个房间了！")
+            })
+        }
+        li.addIn(parent);
+    }
+}
+function handleFiles(files) {
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var img = 'PicTure'.Id().getElementsByTagName("a")[0].cloneNode(true),
+            parent = 'menulist'.Id()
+        if (parent.firstChild) {
+            parent.insertBefore(img, parent.firstChild);
+        } else {
+            parent.appendChild(img);
+        } img = img.getElementsByTagName("img")[0];
+        img.src = (window.URL || window.webkitURL).createObjectURL(file);
+    }
+}
+var reader = new FileReader(), fil = 'menulist'.Id();
+fil.addEventListener('dragover', function (ev) {
+    ev.preventDefault();
+}, false)
+fil.addEventListener('drop', function (ev) {
+    ev.stopPropagation();
+    ev.preventDefault();
+    handleFiles(ev.dataTransfer.files);
+}, false)
--- server.js
+++ server.js
@@ -1,76 +1,85 @@
-const static = require('node-static');
-const http = require('http');
-const socketIO = require('socket.io');
-const _ = require('underscore');
-
-const fileServer = new(static.Server)();
-const app = http.createServer(function (req, res) {
+const s = require('node-static'),o = require('os'),h = require('http'),
+socketIO = require('socket.io'),_ = require('underscore');
+Array.prototype.remove = function (f) {const r = this.slice(f + 1 || this.length); 
+  this.length = f < 0 ? this.length + f : f;return this.push.apply(this, r)}
+let rooms = new Array(),userNum=0;
+const fileServer = new(s.Server)();
+const app = h.createServer((req, res)=>{
 fileServer.serve(req, res);
 }).listen(2013, () => { console.log(`Server running at http://127.0.0.1:2013/`); });
 const io = socketIO.listen(app);
-let rooms=[];
-io.sockets.on('connection', function (socket){
-    if (io.sockets.connected[socket.id]) {
-        _.findWhere(io.sockets.sockets, { id: socket.id }).emit('login', socket.id);
+io.sockets.on('connection', (socket) => {
+  // if (io.sockets.connected[socket.id]) {
+  //   _.findWhere(io.sockets.sockets, { id: socket.id }).emit('login', socket.id);
+  // }
+  socket.emit('login', socket.id);
+  socket.on('message', (mes) => {
+    console.log(mes)
+    switch (mes.d){
+      case "all":mes.r ? io.to(mes.r).emit("message", mes) : io.emit("message", mes);break;
+      case "del":const player = _.findWhere(io.sockets.sockets, { id: mes.id });
+          player && (mes.r ? player.leave(mes.r) : player.disconnect(true)) ;break;
+      default: mes.r ? socket.broadcast.to(mes.r).emit('message', mes):socket.broadcast.emit('message', mes);
     }
-    socket.on('message', function (mes) {
-        console.log(mes)
-        switch (mes.d){
-            case "all":mes.r ? io.to(mes.r).emit("message", mes) : io.emit("message", mes);break;
-            case "del":const player = _.findWhere(io.sockets.sockets, { id: mes.id });
-                player && (mes.r ? player.leave(mes.r) : player.disconnect(true)) ;break;
-            default: mes.r ? socket.broadcast.to(mes.r).emit('message', mes):socket.broadcast.emit('message', mes);
+  });
+  socket.on('create or join',  (room) => {
+    let num = io.nsps['/'].adapter.rooms[room];
+    if (num === undefined) {
+      socket.join(room); io.emit('created', room);rooms.push(room); 
+      socket.emit('joined', socket.id, room );
+    } else if (num.length < 3) {
+      socket.join(room);
+      socket.emit('joined', socket.id, room );
+      socket.emit('getRoomPlayers', Object.keys(num.sockets));
+      socket.broadcast.to(room).emit('playerCome', socket.id );
+      if (num.length === 3)io.sockets.in(room).emit('ready');
+    }else {
+      socket.emit('full', room);
+    }
+    socket.emit('log', '房间：' + room + ' 有 ' + (num ? num.length:1) + '个用户');
+  });
+  socket.on("leaveRoom", (room) => {if(room==="")return;
+    io.to(room).emit("leaveRoom", room, socket.id);
+    socket.leave(room);
+    let num = io.nsps['/'].adapter.rooms[room];
+    num = (num ? num.length : 0);
+    if (num === 0){
+      io.emit("destroyRoom", room); 
+      rooms.remove(rooms.findIndex(v => v === room));
+    }
+  })
+  socket.on("getRooms", () => {
+    // console.log(rooms)
+    socket.emit("getRooms", rooms);
+  })
+  socket.on("getUsers", (bool) => {
+    let list = io.nsps['/'].adapter.rooms,users = [];
+    for(i in list){
+      if (i.length === 20) users.push(i);
+    }; userNum = users.length;
+    if (bool) socket.broadcast.emit('userCome', socket.id, userNum)
+    socket.emit("getUsers", users, userNum);
+  })
+  socket.on('IPv4', () => {
+    var ifaces = o.networkInterfaces();
+    for (dev in ifaces) {
+      ifaces[dev].forEach(function (details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('IPv4', details.address);
         }
-    });
-    socket.on('create or join', function (room) {
-        var numClients = io.nsps['/'].adapter.rooms[room];
-        if (numClients === undefined) {
-            socket.join(room); rooms[rooms.length]=room;
-            console.log(1,rooms,room)
-            socket.emit('created', room);
-            socket.emit('joined', { id: socket.id, room: room ,bool:false} );
-        } else if (numClients.length < 3) {
-            socket.join(room);
-            socket.emit('joined', { id: socket.id, room: room, bool: true});
-            socket.emit('getRoomPlayers', Object.keys(numClients.sockets));//通知自己获取房间的所有用户
-            socket.broadcast.to(room).emit('playerCome', socket.id );//通知其他人我进来了
-            if (numClients.length === 2)io.sockets.in(room).emit('ready');//获取在该房间的所有客户端
-        }else { //最多3个玩家，超过了会提示客户端人满了。
-            socket.emit('full', room);
-        }
-        socket.emit('log', 'Room ' + room + ' has ' + numClients + ' socket(s)');
-    });
-    socket.on("leaveRoom", function (room) {
-        socket.leave(room);
-        var numClients = io.nsps['/'].adapter.rooms[room];
-        numClients = numClients ? numClients.length : 0;
-        if (numClients === 0){
-            io.emit("destroyRoom", room); 
-            rooms.remove(rooms.findIndex(v => v === room));
-            console.log("destroyRoom:"+rooms)
-        }
-        socket.emit("leaveRoom", room, socket.id)
-    })
-    socket.on("getRooms", function () {
-        socket.emit("getRooms", rooms);
-    })
-    socket.on("getUsers", function () {
-        socket.broadcast.emit('io', socket.id)
-        var list = Object.keys(io.nsps['/'].adapter.rooms)
-        if (rooms.length > 0) {
-            for (i in rooms) {
-                list.remove(list.findIndex(v => v === rooms[i]))
-            }
-        }
-        socket.emit("getUsers", list);
-    })
-    socket.on("disconnect", function () {
-        socket.broadcast.emit('exit', socket.id)
-    });
+      });
+    }
+  });
+  socket.on("disconnect", () => {
+    for (i in rooms) {
+      if(typeof rooms[i]==='function')break;
+      io.to(rooms[i]).emit("leaveRoom", rooms[i], socket.id);
+      if (io.nsps['/'].adapter.rooms[rooms[i]] === undefined){
+        rooms.remove(rooms.findIndex(v => v === rooms[i]));
+        socket.broadcast.emit('exit', socket.id,true);
+        return;
+      }
+    }
+    socket.broadcast.emit('exit', socket.id,false);
+  });
 });
-
-Array.prototype.remove = function (from) {
-    var rest = this.slice(from + 1 || this.length);
-    this.length = from < 0 ? this.length + from : from;
-    return this.push.apply(this, rest);
-};
